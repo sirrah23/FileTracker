83bfad17f3a5fda276ce3d333548579e8f3b0a48
sirrah23 <hseth93@gmail.com>
2018-01-13 14:54:36 -0500

    Moved file-status field to FileEntity and updated admin

--- filetracker/tracker/admin.py
+++ filetracker/tracker/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 from .models import FileEntity, FileHistory
 
 
-admin.site.register(FileEntity)
-admin.site.register(FileHistory)
+class FileHistoryInline(admin.TabularInline):
+    list_display = ('content_hash', 'client_modified', 'server_modified')
+    model = FileHistory
+    ordering = ['-inserted']
+
+
+@admin.register(FileEntity)
+class FileEntityAdmin(admin.ModelAdmin):
+    inlines = [FileHistoryInline]
--- filetracker/tracker/models.py
+++ filetracker/tracker/models.py
@@ -6,8 +6,20 @@ class FileEntity(models.Model):
     """
     Model containing the list of files that are being tracked
     """
+    FILE_STATUS = (
+        ('n', 'Non-existent'),
+        ('t', 'Tracked'),
+        ('m', 'Modified')
+    )
+
     name = models.CharField(max_length=200,
                             help_text="Enter a file name")
+
+    status = models.CharField(max_length=1,
+                              choices=FILE_STATUS,
+                              blank=True,
+                              default='m',
+                              help_text='Status of the file')
 
     def get_absolute_url(self):
         return reverse('file-entity-detail', args=[str(self.id)])
@@ -18,29 +30,19 @@ class FileEntity(models.Model):
 
 class FileHistory(models.Model):
 
-    FILE_STATUS = (
-        ('n', 'Non-existent'),
-        ('t', 'Tracked'),
-        ('m', 'Modified')
-    )
-
     file_entity = models.ForeignKey('FileEntity',
                                     on_delete=models.CASCADE,
                                     null=False,
                                     related_name='history')
 
-    client_modified = models.DateField(null=True,
-                                       blank=True)
+    client_modified = models.DateTimeField(null=True,
+                                           blank=True)
 
-    server_modified = models.DateField(null=True,
-                                       blank=True)
+    server_modified = models.DateTimeField(null=True,
+                                           blank=True)
+
+    inserted = models.DateTimeField(auto_now=True)
 
     content_hash = models.CharField(max_length=200,
                                     null=True,
                                     blank=True)
-
-    status = models.CharField(max_length=1,
-                              choices=FILE_STATUS,
-                              blank=True,
-                              default='m',
-                              help_text='Status of the file')
