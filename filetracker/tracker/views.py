198d131ab78196628971e2e5bdaef034212094a4
sirrah23 <hseth93@gmail.com>
2018-01-18 23:15:14 -0500

    Added re-ordering of files by priority based on status

--- filetracker/tracker/views.py
+++ filetracker/tracker/views.py
@@ -16,8 +16,12 @@ class FileEntityListView(generic.ListView):
     # TODO: Access the foreign FileHistory data
     model = FileEntity
     context_object_name = "file_entity_list"
-    queryset = FileEntity.objects.all()
     template_name = 'index.html'
+
+    def get_queryset(self):
+        qs = self.model._default_manager.get_queryset()
+        order = ['m', 't', 'p', 'n']
+        return sorted(qs, key=lambda q: order.index(q.status))
 
 
 class FileEntityDetailView(generic.DetailView):
